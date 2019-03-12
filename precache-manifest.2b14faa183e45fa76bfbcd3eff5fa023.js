29c2ff16f8420f3a0a36adc0fc78f41eef8c32f0
agusmafud <agusmafud@gmail.com>
2019-03-11 22:56:15 -0300

    Updates

---
+++ precache-manifest.2b14faa183e45fa76bfbcd3eff5fa023.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/velvet/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "34be434a410dc4a2ddfc",
+    "url": "/velvet/static/js/runtime~main.87ce0b60.js"
+  },
+  {
+    "revision": "9fe45f1bedd1fcca8fef",
+    "url": "/velvet/static/js/main.f812af9a.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/velvet/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "9fe45f1bedd1fcca8fef",
+    "url": "/velvet/static/css/main.3d2e49c9.chunk.css"
+  },
+  {
+    "revision": "f1708dfab14179998705865621899868",
+    "url": "/velvet/index.html"
+  }
+];
