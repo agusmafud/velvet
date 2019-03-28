1b7b2b73ebbd163a30adba88b7b8ca15b1dce658
agusmafud <agusmafud@gmail.com>
2019-03-28 01:17:13 -0300

    Update

--- src/config/dev.js
+++
@@ -1,5 +0,0 @@
-export const FirebaseConfig = {
-    apiKey: "AIzaSyDuVqWtpwEiM30dNORVgx3_cocbEGY7cKY",
-    authDomain: "velvetchocolates.firebaseapp.com",
-    databaseURL: "https://velvetchocolates.firebaseio.com",
-};
--- src/config/firebase.js
+++
@@ -1,9 +0,0 @@
-import firebase from 'firebase/app';
-import 'firebase/database';
-
-import { FirebaseConfig } from "../config/keys";
-firebase.initializeApp(FirebaseConfig);
-
-const databaseRef = firebase.database().ref();
-export const pedidosRef = databaseRef.child("pedidos");
-export const timestamp = firebase.database.ServerValue.TIMESTAMP;
